41a9e34b3f960b11f69dbdbcc5ea45805ea43d46
atul-morchhlay <atulmorchhlay204@gmail.com>
2023-05-21 19:16:59 +0530

    Fixed dark theme issues

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,9 +8,9 @@ interface ButtonProps {
 
 const Button: FC<ButtonProps> = ({ children, onClick, variant = "primary" }) => {
     return variant === "primary" ? (
-        <button onClick={onClick} className='bg-main px-4 text-background-secondary border-[2px] border-main hover:bg-background-secondary hover:text-main py-1 rounded-md'>{children}</button>
+        <button onClick={onClick} className='bg-main whitespace-nowrap font-semibold px-4 text-background-secondary border-[2px] border-main hover:bg-background-secondary hover:text-main py-1 rounded-md'>{children}</button>
     ) : (
-        <button onClick={onClick} className='bg-background-secondary px-4 text-main border-[2px] border-main hover:bg-main hover:text-background-secondary py-1 rounded-md'>{children}</button>
+        <button onClick={onClick} className='bg-background-secondary whitespace-nowrap font-semibold px-4 text-main border-[2px] border-main hover:bg-main hover:text-background-secondary py-1 rounded-md'>{children}</button>
     )
 }
 
