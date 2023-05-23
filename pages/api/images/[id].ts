70656b7f28f31b955526a0a1eea93e5ee5213b9e
atul-morchhlay <atulmorchhlay204@gmail.com>
2023-05-23 19:35:47 +0530

    Added try catch in apis

--- pages/api/images/[id].ts
+++ pages/api/images/[id].ts
@@ -11,23 +11,33 @@ export const config = {
   },
 };
 
+interface errorType {
+  message: string
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const db:Db = await getDb();
   const { id }= req.query;
 
-  console.log(req.method)
-  const image = await db.collection("Image-uploader").findOne({
-    _id: new ObjectId(id as string)
-  });
-  
-  let htmlContent = `<h1>Some Went Wrong! Image not found.</h1>`;
-  if(image){
-    htmlContent = `
-    <div style="background-color: black; height: 99vh; overflow: none; display: flex; margin: -10px; justify-content: center; align-items: center;">
-      <img src=${image.url} style="height: 90vh; width: auto;" alt=${image._id} />
-    </div>`
+  try {
+    const image = await db.collection("Image-uploader").findOne({
+      _id: new ObjectId(id as string)
+    });
+    
+    let htmlContent = `<h1>Some Went Wrong! Image not found.</h1>`;
+    if(image){
+      htmlContent = `
+      <div style="background-color: black; height: 99vh; overflow: none; display: flex; margin: -10px; justify-content: center; align-items: center;">
+        <img src=${image.url} style="height: 90vh; width: auto;" alt=${image._id} />
+      </div>`
+    }
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send(htmlContent);
+  } catch (error: unknown) {
+    if(error instanceof Error){
+      res.status(400).json({message: error.message})
+    }
   }
-  
-  res.setHeader('Content-Type', 'text/html');
-  res.status(200).send(htmlContent);
+
 }
--- pages/api/images/upload.ts
+++ pages/api/images/upload.ts
@@ -13,14 +13,22 @@ export const config = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === "POST"){
-        const db:Db = await getDb();
-        const addedImage = await db.collection("Image-uploader").insertOne({
-            url: req.body.url 
-        });
-      
-        res.status(200).json({
-            url: `${process.env.DOMAIN_URL}/api/images/${addedImage.insertedId}`
-        });
+        try {
+            const db:Db = await getDb();
+            const addedImage = await db.collection("Image-uploader").insertOne({
+                url: req.body.url 
+            });
+          
+            res.status(200).json({
+                url: `${process.env.DOMAIN_URL}/api/images/${addedImage.insertedId}`
+            });
+        } catch (error) {
+            if(error instanceof Error){
+                res.status(400).json({
+                    message: error.message
+                })
+            }
+        }
     } else {
         res.json({
             message: "Nothing to see here"
